003a81245d795a9c5f55dc7d7f26953d447f92dd
SE TO HO MAN ERIC <EHMSETO_A01@landsd.hksarg>
2026-03-09 10:12:29 +0800

    develop in smo

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -16,13 +16,13 @@ DATABASE_URL = os.getenv(
 
 
 # MongoDB connection string in 3dm-db4
-# MONGODB_URL = os.getenv(
-#     "MONGODB_URL",
-#     "mongodb://10.77.159.237:27017/?replicaSet=rs0"  # "mongodb://3dm-db4:27017/?replicaSet=rs0" # It is a special DNS name automatically provided by Docker Desktop (Mac & Windows).
-# )
-
-# MongoDB connesztion string in local mac machine
 MONGODB_URL = os.getenv(
     "MONGODB_URL",
-    "mongodb://host.docker.internal:27018"  # "mongodb://3dm-db4:27017/?replicaSet=rs0" # It is a special DNS name automatically provided by Docker Desktop (Mac & Windows).
+    "mongodb://10.77.159.237:27017/?replicaSet=rs0"  # "mongodb://3dm-db4:27017/?replicaSet=rs0" # It is a special DNS name automatically provided by Docker Desktop (Mac & Windows).
 )
+
+# MongoDB connesztion string in local mac machine
+# MONGODB_URL = os.getenv(
+#     "MONGODB_URL",
+#     "mongodb://host.docker.internal:27018"  # "mongodb://3dm-db4:27017/?replicaSet=rs0" # It is a special DNS name automatically provided by Docker Desktop (Mac & Windows).
+# )
